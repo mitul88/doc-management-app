7d71f970fd77ec5a6a836f5f03ba43b97b685bec
mitul88 <shahed.rahman88@gmail.com>
2024-11-28 17:34:28 +0600

    env setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log(`Hello, ${process.env}`);
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${process.env.PORT}`);
 });
